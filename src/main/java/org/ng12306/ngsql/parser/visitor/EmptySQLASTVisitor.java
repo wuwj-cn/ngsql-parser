42116e138427f3dfbeef6927a883eeb334f5b0ba
wuwj-cn <wenjie.0617@gmail.com>
2013-03-25 18:52:12 +0800

    delete EmptySQLASTVisitor

--- src/main/java/org/ng12306/ngsql/parser/visitor/EmptySQLASTVisitor.java
+++
@@ -1,9 +0,0 @@
-package org.ng12306.ngsql.parser.visitor;
-
-
-/**
- * @author <a href="mailto:shuo.qius@alibaba-inc.com">QIU Shuo</a>
- */
-public class EmptySQLASTVisitor implements SQLASTVisitor {
-
-}
